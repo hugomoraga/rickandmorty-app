32bf31b0fbd29a4533b18aa7e9cf7f9e517a45b7
Hugo <hg.moraga@gmail.com>
2020-06-17 02:09:06 -0400

    Creacion de Estructuras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CharacterComponent } from './components/character/components/character/character.component';
+import { CharacterListComponent } from './components/character/components/character-list/character-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { EpisodeComponent } from './components/episode/episode/episode.component';
+import { EpisodeListComponent } from './components/episode/episode-list/episode-list.component';
+import { BannerComponent } from './components/home/banner/banner.component';
+import { SearchCharacterComponent } from './components/home/component/search-character/search-character.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found/page-not-found.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent,
+    CharacterListComponent,
+    HomeComponent,
+    LayoutComponent,
+    EpisodeComponent,
+    EpisodeListComponent,
+    BannerComponent,
+    SearchCharacterComponent,
+    PageNotFoundComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/home/component/search-character/search-character.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-character',
+  templateUrl: './search-character.component.html',
+  styleUrls: ['./search-character.component.scss']
+})
+export class SearchCharacterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
